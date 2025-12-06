8b6134c0e182e752fb982b361692cc2ef676e0ef
PARADOXnews <achirubashvili@gmail.com>
2025-12-06 17:05:17 +0400

    a

---
+++ packages/react-moveable/src/app/page.jsx
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+
+<Moveable ></Moveable>
+
+
+
+    </div>
+  );
+}
